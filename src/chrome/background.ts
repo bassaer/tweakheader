1fe441150d153de368218ff42d1c29379f4fb03e
Tsubasa Nakayama <app.nakayama@gmail.com>
2020-12-20 16:01:13 +0900

    override ua

--- src/chrome/background.ts
+++ src/chrome/background.ts
@@ -1,9 +1,20 @@
-const tweakHeader = (debail: chrome.webRequest.WebRequestBodyDetails) => {
-    console.log(JSON.stringify(debail));
-}
+const tweakHeader = (detail: chrome.webRequest.WebRequestHeadersDetails) => {
+    const requestHeaders = detail.requestHeaders;
+    if (!requestHeaders) {
+        return { requestHeaders }
+    }
 
-chrome.webRequest.onBeforeRequest.addListener(
+    for (const header of requestHeaders) {
+        if (header.name.toLocaleLowerCase() === 'user-agent') {
+            header.value = 'Mozilla/5.0 (iPhone; CPU iPhone OS 13_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0 Mobile/15E148 Safari/604.1';
+        }
+    }
+
+    return { requestHeaders }
+};
+
+chrome.webRequest.onBeforeSendHeaders.addListener(
     tweakHeader,
     { urls: ['<all_urls>'] },
-    []
+    ['blocking', 'requestHeaders']
 );
